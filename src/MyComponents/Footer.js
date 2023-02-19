66526f71c141f54dd3fc932e44da86588dc6bc7b
patrehere <ppfreelance246@gmail.com>
2023-02-19 15:45:10 +0530

    initia commit

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Footer = () => {
+  
+    return (
+      <footer className="bg-dark text-light py-1" >
+        <p className="text-center py-1.5 mt-3">Copyright @2023 Todolist.com</p>
+      </footer>
+    );
+}
